a1679d6b6f7cc38b6ef46614a8e4a2a1b7ccac38
Parth Sareen <parth0612@live.com>
2017-10-20 00:23:57 -0400

    Add files via upload

---
+++ Pentiometer day 3/Pentiometer day 3.ino
@@ -0,0 +1,15 @@
+#define led 6
+#define knob 0
+void setup()
+{
+pinMode (led, OUTPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+  int val = analogRead (knob);
+  val = map(val, 1, 1024, 1, 255);
+  analogWrite(led, val);
+  
+  Serial.println(val);
+}
---
+++ test_photosensor/test_photosensor.ino
@@ -0,0 +1,26 @@
+#define led 12
+
+
+
+
+
+
+
+
+
+int pot=0;//Define the analog interface 0 connect the photosensitive resistance
+int led=11;//Define digital interface 11 output PWM adjust LED brightness
+int val=0;//Defined variable val
+void setup()
+{
+pinMode(led,OUTPUT);//Define digital interface 11 for output
+Serial.begin(9600);//Set the baud rate to 9600
+}
+void loop()
+{
+val=analogRead(pot);//Read the sensor's analog values and assign to val
+Serial.println(val);//Display Val variable value
+analogWrite(led,val);// Turn on the LED and set the brightness (the maximum value of the PWM output is 255)
+delay (1000);//delay 1 second
+}
+
